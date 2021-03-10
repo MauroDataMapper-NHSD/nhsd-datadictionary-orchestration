decae4bcb6790cc599c4cdb923b1b79b5bcc1785
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-03-10 12:27:16 +0000

    Add unit tests for SecurityService
    Add jest-marbles for observable marble syntax testing

--- src/app/services/security/security.service.spec.ts
+++ src/app/services/security/security.service.spec.ts
@@ -16,16 +16,47 @@
 
 import { TestBed } from '@angular/core/testing';
 import { TestingModule } from '@mdm/modules/testing/testing.module';
+import { MdmResourcesService } from '../mdm-resources/mdm-resources.service';
+import { SignInCredentials, SignInError, UserDetails } from './security.model';
+import { cold } from 'jest-marbles';
 
 import { SecurityService } from './security.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+interface MdmSecurityResourceStub {
+  login: jest.Mock;
+}
+
+interface MdmSessionResourceStub {
+  isApplicationAdministration: jest.Mock;
+}
+
+interface MdmResourcesServiceStub {
+  security: MdmSecurityResourceStub;
+  session: MdmSessionResourceStub
+}
 
 describe('SecurityHandlerService', () => {
   let service: SecurityService;
+  const resourcesStub: MdmResourcesServiceStub = {
+    security: {
+      login: jest.fn(),
+    },
+    session: {
+      isApplicationAdministration: jest.fn()
+    }
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         TestingModule
+      ],
+      providers: [
+        {
+          provide: MdmResourcesService,
+          useValue: resourcesStub
+        }
       ]
     });
     service = TestBed.inject(SecurityService);
@@ -34,4 +65,54 @@ describe('SecurityHandlerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it.each([
+    ['123', 'user@test.com', false],
+    ['456', 'admin@test.com', true]
+  ])
+  ('should sign in user %s %s when admin = %o', (id, userName, isAdmin) => {
+    const credentials: SignInCredentials = { username: userName, password: 'test' };
+    const expectedUser: UserDetails = {
+      id: id,
+      userName: userName,
+      firstName: 'first',
+      lastName: 'last',
+      isAdmin: isAdmin,
+      needsToResetPassword: false,
+      role: '',
+      token: undefined
+    };
+
+    resourcesStub.security.login.mockImplementationOnce(() => cold('--a|', {
+      a: {
+        body: {
+          id: expectedUser.id,
+          emailAddress: expectedUser.userName,
+          firstName: expectedUser.firstName,
+          lastName: expectedUser.lastName
+        }
+      }
+    }));
+
+    resourcesStub.session.isApplicationAdministration.mockImplementationOnce(() => cold('--a|', { 
+      a: {
+        body: {
+          applicationAdministrationSession: expectedUser.isAdmin
+        }
+      }
+    }));
+
+    const expected$ = cold('----a|', { a: expectedUser });
+    const actual$ = service.signIn(credentials);
+
+    expect(actual$).toBeObservable(expected$);
+  })
+
+  it('should throw error if sign in fails', () => {
+    resourcesStub.security.login.mockImplementationOnce(() => cold('--#', null, new HttpErrorResponse({})));
+    
+    const expected$ = cold('--#');
+    const actual$ = service.signIn({ username: 'fail', password: 'fail' });
+    expect(actual$).toBeObservable(expected$);
+  })
 });
