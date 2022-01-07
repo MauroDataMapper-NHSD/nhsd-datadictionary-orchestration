c1d32d59372f5d7fc704e4ac95bd2dff076bc9a2
James Welch <james.welch@cs.ox.ac.uk>
2022-01-07 10:33:16 +0000

    Fix branches to use ids of versioned folders

--- src/app/branches/branch-detail/branch-detail.component.ts
+++ src/app/branches/branch-detail/branch-detail.component.ts
@@ -61,7 +61,7 @@ export class BranchDetailComponent implements OnInit {
     this.dataDictionary
       .getAvailableBranches()
       .subscribe(branches => {
-        this.details = branches.find(b => b.branchName === this.branchName);
+        this.details = branches.find(b => b.id === this.branchName);
       });
   }
 
